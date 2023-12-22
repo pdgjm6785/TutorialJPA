638b6071c2ecce518e180087244f860cbc9ab604
pdgjm6785 <pdgjm6785@gmail.com>
2023-12-22 21:01:13 +0900

    add kadai

---
+++ src/main/java/com/techacademy/CountryController.java
@@ -0,0 +1,87 @@
+package com.techacademy;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable; // 追加
+import org.springframework.web.bind.annotation.PostMapping; // 追加
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam; // 追加
+
+@Controller
+@RequestMapping("country")
+public class CountryController {
+    private final CountryService service;
+
+    public CountryController(CountryService service) {
+        this.service = service;
+    }
+
+    // ----- 一覧画面 -----
+    @GetMapping("/list")
+    public String getList(Model model) {
+        // 全件検索結果をModelに登録
+        model.addAttribute("countrylist", service.getCountryList());
+        // country/list.htmlに画面遷移
+        return "country/list";
+    }
+
+    // ----- 課題；detail画面への遷移ここから -----
+    @GetMapping(value = { "/detail/{code}", "/detail" })
+    public String getCountry(@PathVariable(name = "code", required = false) String code, Model model) {
+        // codeが指定されていたら検索結果、無ければ空のクラスを設定
+        Country country = code != null ? service.getCountry(code) : new Country();
+        // Modelに登録
+        model.addAttribute("country", country);
+        // country/detail.htmlに画面遷移
+        return "country/detail";
+    }
+    // ----- 課題；detail画面への遷移ここまで -----
+    // ----- 更新（追加） -----
+    @PostMapping("/detail")
+    public String postCountry(@RequestParam("code") String code, @RequestParam("name") String name,
+            @RequestParam("population") int population, Model model) {
+        // 更新（追加）
+        service.updateCountry(code, name, population);
+
+        // 一覧画面にリダイレクト
+        return "redirect:/country/list";
+    }
+
+    // ----- 削除画面 -----
+    // ----- 課題；delete画面への遷移ここから -----
+    @GetMapping(value = { "/delete/{code}", "/delete" })
+    public String deleteCountryForm(@PathVariable(name = "code", required = false) String code, Model model) {
+        // codeが指定されていたら検索結果、無ければ空のクラスを設定
+        Country country = code != null ? service.getCountry(code) : new Country();
+        // Modelに登録
+        model.addAttribute("code", code);
+        model.addAttribute("country", country);
+        // country/delete.htmlに画面遷移
+        return "country/delete";
+    }
+    // ----- 課題；delete画面への遷移ここまで-----
+    // ----- 削除 -----
+    @PostMapping("/delete")
+    public String deleteCountry(@RequestParam("code") String code, Model model) {
+        // 削除
+        service.deleteCountry(code);
+
+        // 一覧画面にリダイレクト
+        return "redirect:/country/list";
+    }
+    // ----- 追加:ここまで  -----
+    // ----- 課題用に追加:ここから -----
+    @RequestMapping("/country/detail/{code}")
+    public String getCountryDetail(@PathVariable String code, Model model) {
+        Country country = service.getCountry(code);
+
+        if (country != null) {
+            model.addAttribute("country", country);
+            return "country/detail";
+        } else {
+            return "error"; // エラーページにリダイレクト
+        }
+    // ----- 課題用に追加:ここまで -----
+    }
+}
